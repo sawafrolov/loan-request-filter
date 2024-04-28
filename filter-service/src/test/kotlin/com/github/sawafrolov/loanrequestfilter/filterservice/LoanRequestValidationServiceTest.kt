145d81670f5dd3ab4f3d1c06d022715b0bf7bfcf
sawafrolov <frolov@devs-vault.com>
2024-04-28 23:47:37 +0400

    АФТ

--- filter-service/src/test/kotlin/com/github/sawafrolov/loanrequestfilter/filterservice/LoanRequestValidationServiceTest.kt
+++ filter-service/src/test/kotlin/com/github/sawafrolov/loanrequestfilter/filterservice/LoanRequestValidationServiceTest.kt
@@ -29,6 +29,64 @@ class LoanRequestValidationServiceTest(
     @Test
     @DisplayName("Test company incorrect")
     fun testCompanyIncorrect() {
-        assertTrue(loanRequestValidationService.checkStopFactors(companyIncorrectDto()).isEmpty())
+        val stopFactors = loanRequestValidationService.checkStopFactors(companyIncorrectDto())
+        assertFalse(stopFactors.isEmpty())
+        assertEquals(3, stopFactors.size)
+        assertTrue(stopFactors.contains("Кредиты выдаются компаниям не более чем на 6 месяцев"))
+        assertTrue(stopFactors.contains("Кредиты выдаются компаниям не более чем на 20 миллионов рублей"))
+        assertTrue(stopFactors.contains("Капитал компании должен превышать 40 процентов от запрашиваемой суммы"))
+    }
+
+    @Test
+    @DisplayName("Test human correct")
+    fun testHumanCorrect() {
+        assertTrue(loanRequestValidationService.checkStopFactors(humanCorrectDto()).isEmpty())
+    }
+
+    @Test
+    @DisplayName("Test human border values")
+    fun testHumanBorderValues() {
+        assertTrue(loanRequestValidationService.checkStopFactors(humanBorderValuesDto()).isEmpty())
+    }
+
+    @Test
+    @DisplayName("Test human incorrect")
+    fun testHumanIncorrect() {
+        val stopFactors = loanRequestValidationService.checkStopFactors(humanIncorrectDto())
+        assertFalse(stopFactors.isEmpty())
+        assertEquals(3, stopFactors.size)
+        assertTrue(stopFactors.contains("Кредиты выдаются физическим лицам не более чем на 24 месяца"))
+        assertTrue(stopFactors.contains("Кредиты выдаются физическим лицам не более чем на 2 миллиона рублей"))
+        assertTrue(stopFactors.contains("Капитал физического лица должен превышать 10 процентов от запрашиваемой суммы"))
+    }
+
+    @Test
+    @DisplayName("Test krasnoyarsk company correct")
+    fun testKrasnoyarskCompanyCorrect() {
+        assertTrue(loanRequestValidationService.checkStopFactors(krasnoyarskCompanyCorrectDto()).isEmpty())
+    }
+
+    @Test
+    @DisplayName("Test krasnoyarsk company border values")
+    fun testKrasnoyarskCompanyBorderValues() {
+        assertTrue(loanRequestValidationService.checkStopFactors(krasnoyarskCompanyBorderValuesDto()).isEmpty())
+    }
+
+    @Test
+    @DisplayName("Test krasnoyarsk company incorrect")
+    fun testKrasnoyarskCompanyIncorrect() {
+        val stopFactors = loanRequestValidationService.checkStopFactors(krasnoyarskCompanyIncorrectDto())
+        assertFalse(stopFactors.isEmpty())
+        assertEquals(1, stopFactors.size)
+        assertTrue(stopFactors.contains("Капитал компании из Красноярского края должен быть больше 5 миллионов рублей"))
+    }
+
+    @Test
+    @DisplayName("Test non-resident")
+    fun testNonresident() {
+        val stopFactors = loanRequestValidationService.checkStopFactors(nonResidentDto())
+        assertFalse(stopFactors.isEmpty())
+        assertEquals(1, stopFactors.size)
+        assertTrue(stopFactors.contains("Кредиты не выдаются нерезидентам РФ"))
     }
 }
--- filter-service/src/test/kotlin/com/github/sawafrolov/loanrequestfilter/filterservice/ObjectUtil.kt
+++ filter-service/src/test/kotlin/com/github/sawafrolov/loanrequestfilter/filterservice/ObjectUtil.kt
@@ -4,81 +4,81 @@ import com.github.sawafrolov.loanrequestfilter.commons.dto.LoanRequestCheckDto
 import java.math.BigDecimal
 
 fun companyCorrectDto() = LoanRequestCheckDto(
-    amount = BigDecimal("1_000_000.00"),
+    amount = BigDecimal("1000000.00"),
     term = 3,
     inn = "1212121212",
-    capital = BigDecimal("500_000.00"),
+    capital = BigDecimal("500000.00"),
     regionNumber = 64
 )
 
 fun companyBorderValuesDto() = LoanRequestCheckDto(
-    amount = BigDecimal("20_000_000.00"),
+    amount = BigDecimal("20000000.00"),
     term = 6,
     inn = "1212121212",
-    capital = BigDecimal("8_000_000.00"),
+    capital = BigDecimal("8000000.00"),
     regionNumber = 64
 )
 
 fun companyIncorrectDto() = LoanRequestCheckDto(
-    amount = BigDecimal("25_000_000.00"),
+    amount = BigDecimal("25000000.00"),
     term = 12,
     inn = "1212121212",
-    capital = BigDecimal("8_000_000.00"),
+    capital = BigDecimal("8000000.00"),
     regionNumber = 64
 )
 
 fun humanCorrectDto() = LoanRequestCheckDto(
-    amount = BigDecimal("1_000_000.00"),
+    amount = BigDecimal("1000000.00"),
     term = 18,
     inn = "121212121212",
-    capital = BigDecimal("150_000.00"),
+    capital = BigDecimal("150000.00"),
     regionNumber = 64
 )
 
 fun humanBorderValuesDto() = LoanRequestCheckDto(
-    amount = BigDecimal("2_000_000.00"),
+    amount = BigDecimal("2000000.00"),
     term = 24,
-    inn = "1212121212",
-    capital = BigDecimal("200_000.00"),
+    inn = "121212121212",
+    capital = BigDecimal("200000.00"),
     regionNumber = 64
 )
 
 fun humanIncorrectDto() = LoanRequestCheckDto(
-    amount = BigDecimal("2_500_000.00"),
+    amount = BigDecimal("2500000.00"),
     term = 30,
-    inn = "1212121212",
-    capital = BigDecimal("150_000.00"),
+    inn = "121212121212",
+    capital = BigDecimal("150000.00"),
     regionNumber = 777
 )
 
 fun krasnoyarskCompanyCorrectDto() = LoanRequestCheckDto(
-    amount = BigDecimal("2_000_000.00"),
+    amount = BigDecimal("2000000.00"),
     term = 3,
     inn = "1212121212",
-    capital = BigDecimal("5_500_000.00"),
+    capital = BigDecimal("5500000.00"),
     regionNumber = 24
 )
 
 fun krasnoyarskCompanyBorderValuesDto() = LoanRequestCheckDto(
-    amount = BigDecimal("2_000_000.00"),
+    amount = BigDecimal("2000000.00"),
     term = 3,
     inn = "1212121212",
-    capital = BigDecimal("5_000_000.00"),
+    capital = BigDecimal("5000000.00"),
     regionNumber = 24
 )
 
 fun krasnoyarskCompanyIncorrectDto() = LoanRequestCheckDto(
-    amount = BigDecimal("2_000_000.00"),
+    amount = BigDecimal("2000000.00"),
     term = 3,
     inn = "1212121212",
-    capital = BigDecimal("4_500_000.00"),
+    capital = BigDecimal("4500000.00"),
     regionNumber = 24
 )
 
 fun nonResidentDto() = LoanRequestCheckDto(
-    amount = BigDecimal("1_000_000.00"),
+    amount = BigDecimal("1000000.00"),
     term = 3,
     inn = "9909121212",
-    capital = BigDecimal("500_000.00"),
+    capital = BigDecimal("500000.00"),
     regionNumber = 64
 )
