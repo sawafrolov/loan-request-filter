9c6d87bb0efe53062ea1f95c735336a7d7e90a62
sawafrolov <frolov@devs-vault.com>
2024-04-29 23:03:10 +0400

    Рефакторинг

--- loan-request-filter-starter/src/main/kotlin/com/github/sawafrolov/loanrequestfilter/starter/jpa/JpaConfiguration.kt
+++ loan-request-filter-starter/src/main/kotlin/com/github/sawafrolov/loanrequestfilter/starter/jpa/JpaConfiguration.kt
@@ -1,8 +1,10 @@
 package com.github.sawafrolov.loanrequestfilter.starter.jpa
 
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@ComponentScan
 @EntityScan("com.github.sawafrolov.loanrequestfilter.commons.entities")
 class JpaConfiguration
